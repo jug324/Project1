71657f59a4bae94f0e2651b9484e5881777c6fb5
Justin Gilman <justingilman@g.harvard.edu>
2014-09-13 16:48:05 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Live URL
+<http://p1.justingilman.me/>
+
+# Description
+This first project is to create a web page that will host links for the remaining projects of the course, as well as provides some personal information.
+
+# Demo
+There is no demo requirement for Project 1.
+
+# Details for teaching team
+N/A
+
+# Outside code
+N/A
+
---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<link rel='stylesheet' href="p1styles.css"/>
+<meta charset="UTF-8">
+<title>Homepage</title>
+</head>
+
+<body>
+<div id="container">
+<h1>Justin Gilman</h1>
+<h1>Dynamic Web Applications Portfolio</h1>
+<img class="self" src="images/self.jpeg" alt="Justin Gilman"/>
+
+<div class="projects">
+<h2>Projects</h2>
+<ul>
+<li><p><a href="http://www.p2.justingilman.me" target="_blank">Project 2</a> | <a href="https://github.com/jug324/p2" target="_blank">Github</a></p></li>
+<li><p><a href="http://www.p3.justingilman.me" target="_blank">Project 3</a> | <a href="https://github.com/jug324/p3" target="_blank">Github</a></p></li>
+<li><p><a href="http://www.p4.justingilman.me" target="_blank">Project 4</a> | <a href="https://github.com/jug324/p4" target="_blank">Github</a></p></li>
+</ul>
+</div>
+
+<div class="questions">
+<h2>In 2+ sentences, describe who you are and what you do.</h2>
+<p>I'm a Vermont raised, Northeastern graduate who currently lives out in the suburbs of Boston in Arlington. I currently work for Harvard in the Alumni Records Office, where I specialize in processing foreign data. Although I was an avid musician and composer throughout school, I now tend to spend the majority of my free time writing a series of novels.</p>
+
+<h2>Why are you taking this course?</h2>
+<p>I took an interest in computer science and technology after I interned at a local start-up (before coming to Harvard). Considering the wonderful resources Harvard offers its employees, in this case through the Extension School, I decided to pursue an education revolving web technologies. I've spent the last few semesters working towards the Web Technologies professional certificate and decided to venture further into web applications.</p>
+<h2>What previous experience do you have with programming?</h2>
+<p>The majority of previous education and experience has been focused on creating web pages/sites (using html, xml, css, etc.). This semester, I am also taking a class focused on java.</p>
+<h2>Are you working on a Mac, PC, or some other platform?</h2>
+<p>My main work computer is a Mac Mini running running OX X version 10.6.8.</p>
+</div>
+</div>
+</body>
+</html>
